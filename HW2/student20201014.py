200eb4427525f0f0fa444ff9ff669cb691098879
Eunjeong-Jang <njej1231@naver.com>
2022-10-09 22:33:27 +0900

    hw2 code

--- HW2/student20201014.py
+++ HW2/student20201014.py
@@ -1,4 +1,62 @@
 #!/usr/bin/python3
 
-print('hello')
+from openpyxl import load_workbook
 
+wb = load_workbook(filename = 'student.xlsx')
+ws = wb['Sheet1']
+
+#total 계산
+row_num = 1
+total = []
+for row in ws:
+	if row_num != 1:
+		sum = ws.cell(row = row_num, column = 3).value * 0.3
+		sum += ws.cell(row = row_num, column = 4).value * 0.35
+		sum += ws.cell(row = row_num, column = 5).value * 0.34
+		sum += ws.cell(row = row_num, column = 6).value
+		ws.cell(row = row_num, column = 7).value = sum
+		
+		total.append(sum)
+	row_num += 1
+
+#grade 계산
+sorted_total = sorted(total, reverse=True) #점수 내림차순 정렬
+max_grade = [] #몇 등까지 해당 학점(A+ ~ C+)을 받을 수 있는지
+max_grade.append((int)(len(total) * 0.15))
+max_grade.append((int)(len(total) * 0.3))
+max_grade.append((int)(len(total) * 0.5))
+max_grade.append((int)(len(total) * 0.7))
+max_grade.append((int)(len(total) * 0.85))
+print(total)
+print(sorted_total)
+print(max_grade)
+
+index = 0
+while index < 5:
+	if max_grade[index] + 1 <= len(total):
+		if sorted_total[max_grade[index]] == sorted_total[max_grade[index] + 1]:
+			max_grade[index] = 1000 #받을 수 없는 학점에는 1000 입력
+		else:
+			max_grade[index] = sorted_total[max_grade[index] - 1]
+		
+	index += 1
+print(max_grade)
+
+row_num2 = 1
+for row in ws:
+	if row_num2 != 1:
+		if ws.cell(row = row_num2, column = 7).value >= max_grade[0]:
+			ws.cell(row = row_num2, column = 8).value = 'A+'
+		elif ws.cell(row = row_num2, column = 7).value >= max_grade[1]:
+			ws.cell(row = row_num2, column = 8).value = 'A0'
+		elif ws.cell(row = row_num2, column = 7).value >= max_grade[2]:
+			ws.cell(row = row_num2, column = 8).value = 'B+'
+		elif ws.cell(row = row_num2, column = 7).value >= max_grade[3]:
+			ws.cell(row = row_num2, column = 8).value = 'B0'
+		elif ws.cell(row = row_num2, column = 7).value >= max_grade[4]:
+			ws.cell(row = row_num2, column = 8).value = 'C+'
+		else:
+			ws.cell(row = row_num2, column = 8).value = 'C0'
+	row_num2 += 1
+
+wb.save("student.xlsx")
