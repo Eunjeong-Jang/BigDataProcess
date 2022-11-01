8ebe6770f2409a81ec27f2447606769ab3071e31
Eunjeong-Jang <njej1231@naver.com>
2022-11-01 14:50:40 +0900

    add IMDB code

--- HW3/IMDBStudent20201014.py
+++ HW3/IMDBStudent20201014.py
@@ -7,11 +7,12 @@ outputFile = sys.argv[2]
 
 with open(inputFile, "rt") as fp:
 	for line in fp:
-		line = line[:-2]
 		movie = line.split("::")
 		genre = movie[2].split("|")
-		print(genre)
 		for g in genre:
+			if g.endswith("\n"):
+				g = g[:-1]
+
 			if g not in movieDict:
 				movieDict[g] = 1
 			else:
@@ -20,7 +21,3 @@ with open(inputFile, "rt") as fp:
 with open(outputFile, "wt") as fp:
 	for m in movieDict:
 		fp.write(m + " " + str(movieDict[m]) + "\n")
-
-with open(outputFile, "rt") as fp:
-	for line in fp:
-		print(fp.read())
